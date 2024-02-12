27a025dc6a0c6c0e6aeaf43158ab3994c50e55b5
Seunghyeok Kim <shkim8745@gmail.com>
2024-02-12 08:08:09 -0800

    CSS and Javascript upload

--- guess-game.js
+++ guess-game.js
@@ -5,6 +5,7 @@
 // random num > user num Up sign!!
 // reset button game reset
 // 5 chances and game disabled
+//  show correct answer
 // notify user only 1~100 numbers allowed and not losing chance
 // user cannot input the same number already put and not losing chance
 
@@ -12,9 +13,25 @@ let computerNum = 0;
 let playButton = document.getElementById('play-button');
 let userInput = document.getElementById('user-num');
 let userResult = document.getElementById('result');
+let resetButton = document.getElementById('reset-button');
+let chances = 5;
+let chanceArea = document.getElementById('chance');
+let history = [];
+let answerArea = document.getElementById('answer');
+let boo = document.getElementById('win-picture');
+let gameName = document.getElementById('main-title');
+let gameType = document.getElementById('subtitle');
+let gameBox = document.getElementById('mainBox');
+let parentElement = gameName.parentNode;
+let parentResult = userResult.parentNode;
+let parentReset = resetButton.parentNode;
 
 playButton.addEventListener('click', play);
-
+resetButton.addEventListener('click', reset);
+userInput.addEventListener('focus', function () {
+  userInput.value = '';
+});
+// focus는 커서가
 // getElementById - 아이디를 통해 가져오는 방식
 //getElementByClassName, querySelector
 // addEventListener - 이벤트 추가 focus hover etc.
@@ -31,15 +48,89 @@ function pickRandomNumber() {
 }
 
 function play() {
-  let userValue = userInput.value;
-  if (userValue < computerNum) {
-    userResult.textContent = 'Go up!';
-  } else if (userValue > computerNum) {
-    userResult.textContent = 'Go down!';
-  } else if (userValue == computerNum) {
-    userResult.textContent = 'Correct guess!!!';
+  let userValue = userInput.value; // .value 값을 읽어옴
+  userResult.removeAttribute('hidden');
+
+  if (userValue < 1 || userValue > 100) {
+    userResult.textContent = 'PUT 1 ~ 100 number..';
+    return;
+    // 이후의 값은 실행시키지 않음
   }
-  // .value 값을 읽어옴
+  if (history.includes(userValue)) {
+    userResult.textContent = 'NO same number..';
+    return;
+  }
+  chances--;
+  chanceArea.textContent = `Chances: ${chances}`;
+  console.log('Chances', chances);
+
+  if (userValue < computerNum) {
+    userResult.textContent = 'GO UP!';
+  } else if (userValue > computerNum) {
+    userResult.textContent = 'GO DOWN!';
+  } else if (userValue == computerNum) {
+    userResult.textContent = `You're lucky!`;
+    playButton.disabled = true;
+    chanceArea.style.display = 'none';
+    userInput.style.display = 'none';
+    playButton.style.display = 'none';
+    boo.style.display = 'block';
+    gameName.style.display = 'none';
+    gameType.style.display = 'none';
+    parentElement.insertBefore(userResult, gameName);
+  }
+
+  history.push(userValue);
+
+  console.log(history);
+  if (chances == 1) {
+    chanceArea.textContent = 'One Last Chance...';
+  }
+  if (chances < 1) {
+    chanceArea.textContent = '';
+    playButton.disabled = true;
+    userResult.textContent = '';
+    chanceArea.style.display = 'none';
+    userInput.style.display = 'none';
+    playButton.style.display = 'none';
+    gameName.style.display = 'none';
+    gameType.style.display = 'none';
+    gameBox.style.backgroundImage = 'none';
+    resetButton.style.position = 'fixed';
+    resetButton.style.bottom = '35px';
+    showFailed();
+  }
+}
+
+function showFailed() {
+  document.body.style.backgroundImage = "url('scary.gif')";
+  document.body.style.backgroundSize = 'cover';
+  document.body.style.backgroundSize = '100% 100%';
+}
+
+function reset() {
+  // user input is clear
+  userInput.value = '';
+  userResult.textContent = '';
+  userInput.style.display = 'block';
+  playButton.style.display = 'block';
+  chanceArea.style.display = 'block';
+  boo.style.display = 'none';
+  gameName.style.display = 'block';
+  gameType.style.display = 'block';
+  gameBox.style.backgroundImage = 'url(dark-hallway.jpg)';
+  document.body.style.backgroundImage = 'none';
+  document.body.style.backgroundSize = 'auto';
+  resetButton.style.position = 'static';
+  resetButton.style.bottom = 'auto';
+  parentResult.insertBefore(userResult, chanceArea);
+  playButton.disabled = false;
+  chances = 5;
+  chanceArea.textContent = `You have ${chances} chances..`;
+  history = [];
+
+  // re-operate number
+  pickRandomNumber();
 }
 
 pickRandomNumber();
